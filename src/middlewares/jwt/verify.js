73f60d8a8deb3b94ba1c7c71647f6d5d550324ec
Developer-Angelo30 <angeloreyes033001@gmail.com>
2024-01-27 15:08:53 +0800

    still cookies

--- src/middlewares/jwt/verify.js
+++ src/middlewares/jwt/verify.js
@@ -3,22 +3,14 @@ require('dotenv').config();
 
 const verifyToken = (req, res, next) => {
   const token = req.cookies.jwt;
-  
+
   jwt.verify(token, process.env.JWT_SECRET_TOKEN, (error, decoded) => {
     if (error) {
-      if (error.name === 'TokenExpiredError') {
-        return res.status(401).json({
+        return res.status(200).json({
           status: false,
-          message: 'Unauthorized: Token has expired',
+          message: 'Unauthorized',
         });
-      } else {
-        return res.status(401).json({
-          status: false,
-          message: 'Unauthorized: Invalid token',
-        });
-      }
     }
-    
     req.decoded = decoded;
     next();
   });
